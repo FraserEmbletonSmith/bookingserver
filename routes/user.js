8097aa3286bb222ce8173d2a6a53c9a30f640d4b
Fraser Embleton-Smith <fraserembletonsmith@yahoo.co.uk>
2016-01-21 23:54:12 +0000

    Fleshes out Create and Read of Users

--- routes/user.js
+++ routes/user.js
@@ -8,4 +8,25 @@ router.get('/', function(req, res, next){
     res.json(bs);
   });
 });
+
+router.get('/:name', function(req, res, next){
+  models.Booking.findAll({
+    include: [{
+      model: models.User,
+      where: {id: models.Sequelize.col('Booking.UserId'),
+              name: req.params.name }
+    }]
+  }).then(function(bookings){
+    res.json(bookings);
+  })
+});
+
+router.get('/create/:name/:email', function(req, res, next){
+  models.User.create({
+    name: req.params.name,
+    email: req.params.email
+  }).then(function(user){
+    res.json(user);
+  });
+});
 module.exports = router
